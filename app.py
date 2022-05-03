885fee75e26d0956a6b77b0cba3d90c684addf9f
Jose Rojas <joseyx041199@gmail.com>
2022-05-03 15:17:49 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def landing_page():  # put application's code here
+    return render_template('index.html', content='test')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', content='test')
+
+
+if __name__ == '__main__':
+    app.run()
