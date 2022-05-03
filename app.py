17185e7effad00f094283594a0f35f93493ddbaf
Jose Rojas <joseyx041199@gmail.com>
2022-05-03 16:06:44 -0400

    comments

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')  # default route
 def landing_page():
     return render_template('index.html', content='test')
 
 
-@app.route('/about')
+@app.route('/about')  # about route
 def about_page():
     return render_template('about.html', content='test')
 
